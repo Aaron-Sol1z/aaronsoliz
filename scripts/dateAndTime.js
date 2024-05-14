7391442945947f018145074a4556a8b2dcebf046
Aaron Soliz <aaronmsoliz@hotmail.com>
2024-05-14 13:40:22 -0700

    Added time and date script

---
+++ scripts/dateAndTime.js
@@ -0,0 +1,92 @@
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.getElementById("clock").textContent = timeString;
+}
+
+function updateDate(){
+    const today = new Date();
+    let dayOfWeek = today.getDay();
+    switch(dayOfWeek){
+        case 1:
+            dayOfWeek = "Monday";
+            break;
+        case 2:
+            dayOfWeek = "Tuesday";
+            break;
+        case 3:
+            dayOfWeek = "Wednesday";
+            break;
+        case 4:
+            dayOfWeek = "Thursday";
+            break;
+        case 5:
+            dayOfWeek = "Friday";
+            break;
+        case 6:
+            dayOfWeek = "Saturday";
+            break;
+        case 7:
+            dayOfWeek = "Sunday";
+            break;
+        default:
+            dayOfWeek = "?";
+            console.log(`${dayOfWeek} is not a valid day of the week`);
+    }
+    let month = (today.getMonth() + 1);
+    switch(month){
+        case 1:
+            month = "January";
+            break;
+        case 2:
+            month = "February";
+            break;
+        case 3:
+            month = "March";
+            break;
+        case 4:
+            month = "April";
+            break;
+        case 5:
+            month = "May";
+            break;
+        case 6:
+            month = "June";
+            break;
+        case 7:
+            month = "July";
+            break;
+        case 8:
+            month = "August";
+            break;
+        case 9:
+            month = "September";
+            break;
+        case 10:
+            month = "October";
+            break;
+        case 11:
+            month = "November";
+            break;
+        case 12:
+            month = "December";
+            break;
+        default:
+            month = "?";
+            console.log(`${month} is not a valid month.`);
+    }
+    const day = today.getDate().toString();
+    const year = today.getFullYear().toString();
+    const dateString = `${dayOfWeek}, ${month} ${day}, ${year}`;
+    document.getElementById("date").textContent = dateString;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+updateDate();
